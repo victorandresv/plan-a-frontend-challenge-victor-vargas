30525f1cc9626e802ebaac9aa354f5db24423c77
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-14 13:01:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Plan a Frontend Challenge Victor Vargas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
